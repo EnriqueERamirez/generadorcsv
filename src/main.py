fd75235e596f67342a2b62d1169ae8792aad8eab
Enrique Ramirez <eramirez.ice@ibm.com>
2023-02-27 23:57:46 -0300

    integracion de bucle

--- src/main.py
+++ src/main.py
@@ -4,21 +4,24 @@ import pdfkit
 from datetime import datetime
 
 my_name = "Frank Andrade"
-item1 = "TV"
-item2 = "Couch"
-item3 = "Washing Machine"
-today_date = datetime.today().strftime("%d %b, %Y")
 
-context = {'my_name': my_name, 'item1': item1, 'item2': item2, 'item3': item3,
-           'today_date': today_date}
+context = {
+        'my_name': my_name,
+        'my_degree': "ingenieriai civil electronica",
+        'my_address': "direccion de prueba",
+        'my_numberphone': "+58 99123123",
+        'my_email': "test@gmail.com",
+        'my_about': "askdjkasjdkasjdkj ajdkjakdjakdjk kasjdlajdoqwiujeokds aksdjaosed",
+        }
 
-template_loader = jinja2.FileSystemLoader('./')
+template_loader = jinja2.FileSystemLoader("template")
 template_env = jinja2.Environment(loader=template_loader)
 
-html_template = 'basic-template.html'
+html_template = 'index.html'
 template = template_env.get_template(html_template)
 output_text = template.render(context)
-
-config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')
-output_pdf = 'pdf_generated.pdf'
-pdfkit.from_string(output_text, output_pdf, configuration=config, css='style.css')
+print(output_text)
+# config = pdfkit.configuration(wkhtmltopdf = r"C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe")
+# config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')
+# output_pdf = 'pdf_generated.pdf'
+# pdfkit.from_string(output_text, output_pdf, configuration=config, css='template/style.css')
